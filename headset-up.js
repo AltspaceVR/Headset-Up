077e83f07e3ba9bac5aae5c8a5e871e58d0ba55a
Steven Vergenz <steven@altvr.com>
2017-03-22 14:32:51 -0700

    Added local controls, synced hud

--- headset-up.js
+++ headset-up.js
@@ -32,7 +32,20 @@ AFRAME.registerComponent('json', {
 	}
 });
 
-AFRAME.registerComponent('hu-question-id', {
+AFRAME.registerComponent('mixin-on', {
+	schema: {
+		on: {type: 'string'},
+		mixins: {type: 'array'}
+	},
+	init: function(){
+		var self = this;
+		this.el.addEventListener(this.data.on, function(){
+			self.el.setAttribute('mixin', self.data.mixins.join(' '));
+		});
+	}
+});
+
+AFRAME.registerComponent('hud-question-id', {
 	dependencies: ['json', 'n-text'],
 	schema: {type: 'array'},
 	update: function(){
@@ -42,17 +55,23 @@ AFRAME.registerComponent('hu-question-id', {
 	}
 });
 
-AFRAME.registerComponent('hu-next-question', {
+AFRAME.registerComponent('hud-next-question', {
 	schema: {
-		target: {type: 'selector'},
 		on: {type: 'string', default: 'click'}
 	},
 	init: function()
+	{
+		this.advanceQuestion = this._advanceQuestion.bind(this);
+		this.el.addEventListener(this.data.on, this.advanceQuestion);
+	},
+	initCategories: function()
 	{
 		function sum(acc, val){
 			return acc + val;
 		}
 
+		var userId = this.el.sceneEl.systems['sync-system'].userInfo.userId;
+		this.target = document.querySelector('[data-creator-user-id="'+userId+'"]');
 		var catString = this.el.sceneEl.dataset.categories;
 		this.catPaths = parseCategories(catString);
 
@@ -61,7 +80,7 @@ AFRAME.registerComponent('hu-next-question', {
 		this.catOffsets =
 			this.catPaths
 			.map(function(name){
-				return getDeepValue(this.data.target.json, name, []).length;
+				return getDeepValue(this.target.json, name, []).length;
 			}, this)
 			.map(function(length, i, array){
 				return array.slice(0, i+1).reduce(sum, 0);
@@ -69,15 +88,16 @@ AFRAME.registerComponent('hu-next-question', {
 
 		// the first item in the offsets list is always zero
 		this.catOffsets.unshift(0);
-
-		this.advanceQuestion = this._advanceQuestion.bind(this);
-		this.el.addEventListener(this.data.on, this.advanceQuestion);
 	},
 	remove: function(){
 		this.el.removeEventListener(this.data.on, this.advanceQuestion);
 	},
 	_advanceQuestion: function()
 	{
+		if(!this.target || !this.catPaths || !this.catOffsets){
+			this.initCategories();
+		}
+
 		var totalLength = this.catOffsets[this.catOffsets.length-1];
 		var newQTotalIndex = Math.floor( Math.random() * totalLength );
 
@@ -91,6 +111,6 @@ AFRAME.registerComponent('hu-next-question', {
 		newQPath.push( newQTotalIndex - this.catOffsets[catIndex] );
 
 		// update the question id with the new name
-		this.data.target.setAttribute('hu-question-id', newQPath);
+		this.target.setAttribute('hud-question-id', newQPath);
 	}
 });
